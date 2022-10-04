3f84783b2d7ecada33a78259aeddf941969e6363
Paulo Rocha <104393414+paulonnn@users.noreply.github.com>
2022-10-04 20:32:13 -0300

    Novos Comandos

--- bottelegram.py
+++ bottelegram.py
@@ -7,6 +7,7 @@ Chave_API = "5449080279:AAEMmhTKME9SZXpDUTn2jSQHbpwsc6uTQGg"
 bot = telebot.TeleBot(Chave_API)
 
 RETORNARFILTRADO_LEN = 18 # '/RetornarFiltrado '
+RETORPRECOSPORESTADO_LEN = 25 # '/RetornarPrecosPorEstado '
 
 #Comando de /RetornarFiltrado
 @bot.message_handler(commands=["RetornarFiltrado"])
@@ -24,8 +25,7 @@ def responder(mensagem):
 
     #Request
     r = requests.get('http://localhost:5000/api/v1/Registro/RetornarFiltrado/{0}/{1}/{2}'.format(estado, municipio, produto))
-    resposta = r.json()
-    print(resposta)    
+    resposta = r.json()    
     respString = "Seus filtros:"
     respString += "\nEstado: " + resposta['estado']
     respString += "\nMunicipio: " + resposta['municipio']
@@ -42,8 +42,7 @@ def responder(mensagem):
 @bot.message_handler(commands=["RetornarMunicipios"])
 def responder(mensagem):
     r = requests.get('http://localhost:5000/api/v1/Registro/RetornarMunicipios')
-    resposta = r.json()
-    print(type(resposta))
+    resposta = r.json()    
     strResp = "Lista de Municípios disponíveis para consulta:\n\n"
 
     for c in resposta:
@@ -55,8 +54,7 @@ def responder(mensagem):
 @bot.message_handler(commands=["RetornarEstados"])
 def responder(mensagem):
     r = requests.get('http://localhost:5000/api/v1/Registro/RetornarEstados')
-    resposta = r.json()
-    print(type(resposta))
+    resposta = r.json()    
     strResp = "Lista de Estados disponíveis para consulta:\n\n"
 
     for c in resposta:
@@ -68,14 +66,36 @@ def responder(mensagem):
 @bot.message_handler(commands=["RetornarCombustiveis"])
 def responder(mensagem):
     r = requests.get('http://localhost:5000/api/v1/Registro/RetornarProdutos')
-    resposta = r.json()
-    print(type(resposta))
+    resposta = r.json()    
     strResp = "Lista de Combustiveis disponíveis para consulta:\n\n"
 
     for c in resposta:
         strResp += c + "\n"
     
     bot.send_message(mensagem.chat.id, strResp)
+
+#Comando de /RetornarPrecosPorEstado
+@bot.message_handler(commands=["RetornarPrecosPorEstado"])
+def responder(mensagem):
+    msg_text = mensagem.text[RETORPRECOSPORESTADO_LEN:]
+    if msg_text == "":
+        msg_text = "Favor inserir todos os parâmetros necessários: /RetornarPrecosPorEstado COMBUSTIVEL"
+        return bot.send_message(mensagem.chat.id, msg_text)
+
+    combustivel = msg_text.upper()
+    print(combustivel)
+
+    r = requests.get('http://localhost:5000/api/v1/Registro/RetornarPrecosPorEstados/{0}'.format(combustivel))
+    resposta = r.json()    
+    respString = "Preço médio de revenda do {0} por estado\n\n".format(combustivel)
+
+    for r in resposta:
+        respString += "\nEstado: " + r['estado']    
+        respString += "\nProduto: " + r['produto']                
+        respString += "\nPreço médio de revenda: " + str(r['preco_medio_revenda'])
+        respString += "\n"
+    
+    bot.send_message(mensagem.chat.id, respString)    
 
 
 
@@ -89,11 +109,12 @@ def verificar(mensagem):
 
 @bot.message_handler(func=verificar) #Só vai retornar caso a func "verificar" retornar True
 def responder(mensagem):
-    texto = """Escolha uma opção para continuar (Clique no item desejado):
+    texto = """Bem-vindo ao Gasolina Bot!\nEscolha uma opção para continuar (Clique no item desejado):
     \n/RetornarMunicipios - Para retornar todos os municipios disponíveis para consulta.
     \n/RetornarEstados - Para retornar todos os estados disponíveis para consulta.
     \n/RetornarCombustiveis - Para retornar todos os combustiveis disponíveis para consulta.
-    \n/RetornarFiltrado ESTADO/MUNICIPIO/COMBUSTIVEL - Para retornar todas as informações de combustivel de maneira filtrada.    
+    \n/RetornarFiltrado ESTADO/MUNICIPIO/COMBUSTIVEL - Para retornar todas as informações de combustivel de maneira filtrada.
+    \n/RetornarPrecosPorEstado COMBUSTIVEL - Para retornar todas os preços médios de revenda do combustivel escolhido por estado.
     \nResponder qualquer outra coisa não vai funcionar, clique em uma das opções ou digite /menu para ver esse menu sempre que desejar."""
     bot.reply_to(mensagem, texto)
 
