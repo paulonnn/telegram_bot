8a2424c47d2034665641e0e4d4c4e1f3ad2dae16
Paulo Rocha <104393414+paulonnn@users.noreply.github.com>
2022-10-12 12:08:34 -0300

    Adição da opção de avaliar o bot

--- bottelegram.py
+++ bottelegram.py
@@ -1,10 +1,14 @@
 from asyncore import dispatcher
+from contextlib import nullcontext
 import string
 from tracemalloc import start
 from telegram import *
 import telegram
 from telegram.ext import *
 import requests
+from datetime import date
+
+DATA_ATUAL = date.today().strftime("%d/%m/%Y")
 
 # Token para funcionamento do BOT
 updater = Updater(token="5449080279:AAEMmhTKME9SZXpDUTn2jSQHbpwsc6uTQGg")
@@ -12,7 +16,7 @@ dispatcher = updater.dispatcher
 
  # Listar Menu com as opções
 def menu(update: Update, context: CallbackContext):
-    buttons = [[InlineKeyboardButton("Municipios", callback_data="municipios-listar")], [InlineKeyboardButton("Estados", callback_data="estados-listar")], [InlineKeyboardButton("Combustiveis", callback_data="combustiveis-listar")]]
+    buttons = [[InlineKeyboardButton("Municipios", callback_data="municipios-listar")], [InlineKeyboardButton("Estados", callback_data="estados-listar")], [InlineKeyboardButton("Combustiveis", callback_data="combustiveis-listar")], [InlineKeyboardButton("Avaliar o BOT", callback_data="opcao-avaliar")]]
     texto = "Bem-vindo ao Gasolina Bot!\nEscolha uma opção para continuar (<b><i>Clique</i></b> no item desejado):"
     return context.bot.send_message(chat_id=update.effective_chat.id, reply_markup=InlineKeyboardMarkup(buttons), text=texto, parse_mode=telegram.constants.PARSEMODE_HTML)
 
@@ -99,7 +103,7 @@ def queryHandlerMunicipios(update: Update, context: CallbackContext, municipio: 
     respString = (f"<b>Preço médio de revenda dos combustiveis referentes ao municipio do {municipio}</b>\n\n")
 
     for resp in resposta:
-        respString += "\n<b>Produto:</b> " + resp['produto']                
+        respString += "\n<b>Produto:</b> " + resp['produto']
         respString += "\n<b>Preço médio de revenda:</b> R$" + str(resp['preco_medio_revenda'])
         respString += "\n"
 
@@ -119,6 +123,39 @@ def queryHandlerCombustiveis(update: Update, context: CallbackContext, combustiv
 
     context.bot.send_message(chat_id=update.effective_chat.id, text=respString, parse_mode=telegram.constants.PARSEMODE_HTML)   
 
+# Pergunta se o usuario deseja mostrar o menu novamente
+def msgAvaliacao(update: Update, context: CallbackContext):
+    buttons = [[InlineKeyboardButton("1 ⭐", callback_data="1-nota")], [InlineKeyboardButton("2 ⭐⭐", callback_data="2-nota")], [InlineKeyboardButton("3 ⭐⭐⭐", callback_data="3-nota")], [InlineKeyboardButton("4 ⭐⭐⭐⭐", callback_data="4-nota")], [InlineKeyboardButton("5 ⭐⭐⭐⭐⭐", callback_data="5-nota")]]
+    context.bot.send_message(chat_id=update.effective_chat.id, reply_markup=InlineKeyboardMarkup(buttons), text="Por favor escolha uma nota de <b>1 a 5</b>", parse_mode=telegram.constants.PARSEMODE_HTML)
+
+
+
+def queryHandlerAvaliar(update: Update, context: CallbackContext, nota: int):
+    userTelegramID = update.callback_query.from_user.id
+
+    objAvaliacao = {'idUsuarioTelegram': str(userTelegramID),
+                    'data' : DATA_ATUAL,
+                    'nota' : nota,
+                    'descricao' : ''}
+
+    r = requests.post('http://localhost:5000/api/v1/Avaliacao', json = objAvaliacao)
+    resposta = r.json()
+
+    nota = resposta['nota']    
+
+    context.bot.send_message(chat_id=update.effective_chat.id, text=(f"A sua avaliação de nota {nota} foi registrada com sucesso. Agradecemos a sua opinião!"), parse_mode=telegram.constants.PARSEMODE_HTML)
+
+def verificarSeJaAvaliou(update: Update, context: CallbackContext):
+    userTelegramID = update.callback_query.from_user.id
+
+    r = requests.get(f'http://localhost:5000/api/v1/Avaliacao/{userTelegramID}')
+    resposta = r.json()    
+
+    if resposta['status'] == 404:
+        return False
+    else:
+        return True        
+
 # Handler das querys (opções clicaveis)
 def queryHandler(update: Update, context: CallbackContext):
     query = update.callback_query.data
@@ -130,26 +167,33 @@ def queryHandler(update: Update, context: CallbackContext):
 
     if tipoComando == "listar":
         if arg1 == "estados":
-            ListarEstados(update, context)            
+            ListarEstados(update, context)
         elif arg1 == "municipios":
-            ListarMunicipios(update, context)            
+            ListarMunicipios(update, context)
         elif arg1 == "combustiveis":
             ListarCombustiveis(update, context)
         elif arg1 == "menu":
-            menu(update, context)
-    else:
-        if tipoComando == "estado": # Caso seja um ESTADO
-            queryHandlerEstados(update, context, arg1)
-            msgMenuNovamente(update, context)
+            menu(update, context)        
+    elif tipoComando == "estado": # Caso seja um ESTADO        
+        queryHandlerEstados(update, context, arg1)
+        msgMenuNovamente(update, context)
 
-        elif tipoComando == "municipio": # Caso seja um MUNICIPIO
-            queryHandlerMunicipios(update, context, arg1)
-            msgMenuNovamente(update, context)
+    elif tipoComando == "municipio": # Caso seja um MUNICIPIO
+        queryHandlerMunicipios(update, context, arg1)
+        msgMenuNovamente(update, context)
 
-        elif tipoComando == "combustiveis": # Caso seja um COMBUSTIVEL
-            queryHandlerCombustiveis(update, context, arg1)
+    elif tipoComando == "combustiveis": # Caso seja um COMBUSTIVEL
+        queryHandlerCombustiveis(update, context, arg1)
+        msgMenuNovamente(update, context)
+    elif tipoComando == "avaliar": # Caso seja para avaliar o bot
+        msgAvaliacao(update, context)
+    elif tipoComando == "nota": # Caso seja para dar a nota de 1 a 5 para o bot        
+        if verificarSeJaAvaliou(update, context) == False:
+            queryHandlerAvaliar(update, context, arg1)
             msgMenuNovamente(update, context)
-
+        else:            
+            context.bot.send_message(chat_id=update.effective_chat.id, text="Você já deu a sua nota para nós, não é permitido mais de uma nota por usuário.", parse_mode=telegram.constants.PARSEMODE_HTML)
+            msgMenuNovamente(update, context)
 
 dispatcher.add_handler(CommandHandler("start", startCommand))
 dispatcher.add_handler(MessageHandler(Filters.text, messageHandler))
